66c1715852f976d5c4a7c764dc247eeee58f61b5
Jeff Statham <Jef.statham@gmail.com>
2023-11-14 12:37:46 -0500

    wip remove duplicate test case

--- test/createAndGrantPermSnapshot.test.ts
+++ test/createAndGrantPermSnapshot.test.ts
@@ -21,50 +21,7 @@ test('Snapshot', () => {
     ],
     description: 'Test Permission Set',
     name: 'TestPermissionSet',
-    ssoInstanceArn: 'arn:aws:sso:::instance/ssoins-1234567891234567',
-    relayStateType: 'https://us-east-1.console.aws.amazon.com/rds/home',
-    sessionDuration: Duration.hours(10),
-    customerManagedPolicyReferences: [
-      {
-        name: 'TestPolicy',
-        path: '/',
-      },
-    ],
-    inlinePolicy: new iam.PolicyDocument({
-      statements: [
-        new iam.PolicyStatement({
-          actions: ['rds:*'],
-          resources: ['*'],
-        }),
-      ],
-    }),
-    permissionsBoundary: {
-      managedPolicyArn: iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonRDSPerformanceInsightsReadOnly').managedPolicyArn,
-    },
-  });
-
-  permissionSet.grant('permissionSetAssignment', {
-    principal: {
-      principalId: '12350630-0ae9-479a-97c2-0afc2d5b4eac',
-      principalType: PrincipalTypes.GROUP,
-    },
-    targetId: '344567890123456',
-  });
-
-  expect(Template.fromStack(stack)).toMatchSnapshot();
-});
-
-test('Snapshot importValue', () => {
-  const app = new App();
-  const stack = new Stack(app, 'TestStack');
-
-  const permissionSet = new PermissionSet(stack, 'PermissionSet', {
-    awsManagedPolicies: [
-      iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonRDSPerformanceInsightsReadOnly'),
-    ],
-    description: 'Test Permission Set',
-    name: 'TestPermissionSet',
-    ssoInstanceArn: Fn.importValue('fake-sso-instance-arn'),
+    ssoInstanceArn: Fn.importValue('path.to.ssoInstanceArn.in.buildConfig'),
     relayStateType: 'https://us-east-1.console.aws.amazon.com/rds/home',
     sessionDuration: Duration.hours(10),
     customerManagedPolicyReferences: [
